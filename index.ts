3b7b0436fbfd9088aa25fae6f70e7fa1ce5b0152
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-01-09 23:37:43 -0500

    basic snake algorithm

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@
 
 import runServer from './server';
 import { GameState, InfoResponse, MoveResponse } from './types';
+import { astar } from './astar';
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
@@ -21,8 +22,8 @@ function info(): InfoResponse {
 
   return {
     apiversion: "1",
-    author: "",       // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
+    author: "dave",       // TODO: Your Battlesnake Username
+    color: "#008080", // TODO: Choose color
     head: "default",  // TODO: Choose head
     tail: "default",  // TODO: Choose tail
   };
@@ -68,14 +69,56 @@ function move(gameState: GameState): MoveResponse {
   }
 
   // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-  // boardWidth = gameState.board.width;
-  // boardHeight = gameState.board.height;
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
+
+  if (myHead.x === 0) {
+    isMoveSafe.left = false;
+  } else if (myHead.x === boardWidth - 1) {
+    isMoveSafe.right = false;
+  }
+
+  if (myHead.y === 0) {
+    isMoveSafe.down = false;
+  } else if (myHead.y === boardHeight - 1) {
+    isMoveSafe.up = false;
+  }
 
   // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  // myBody = gameState.you.body;
+  const myBody = gameState.you.body;
+
+  for (let i = 0; i < myBody.length; i++) {
+    if (myBody[i].x === myHead.x - 1 && myBody[i].y === myHead.y) {
+      isMoveSafe.left = false;
+    } else if (myBody[i].x === myHead.x + 1 && myBody[i].y === myHead.y) {
+      isMoveSafe.right = false;
+    }
+
+    if (myBody[i].y === myHead.y - 1 && myBody[i].x === myHead.x) {
+      isMoveSafe.down = false;
+    } else if (myBody[i].y === myHead.y + 1 && myBody[i].x === myHead.x) {
+      isMoveSafe.up = false;
+    }
+  }
 
   // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-  // opponents = gameState.board.snakes;
+  const opponents = gameState.board.snakes;
+
+  for (let i = 0; i < opponents.length; i++) {
+    const opponentHead = opponents[i].body[0];
+
+    if (opponentHead.x === myHead.x - 1 && opponentHead.y === myHead.y) {
+      isMoveSafe.left = false;
+    } else if (opponentHead.x === myHead.x + 1 && opponentHead.y === myHead.y) {
+      isMoveSafe.right = false;
+    }
+
+    if (opponentHead.y === myHead.y - 1 && opponentHead.x === myHead.x) {
+      isMoveSafe.down = false;
+    } else if (opponentHead.y === myHead.y + 1 && opponentHead.x === myHead.x) {
+      isMoveSafe.up = false;
+    }
+  }
 
   // Are there any safe moves left?
   const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
@@ -88,7 +131,35 @@ function move(gameState: GameState): MoveResponse {
   const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
 
   // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-  // food = gameState.board.food;
+  const food = gameState.board.food;
+
+  // sort food by distance from head
+  food.sort((a, b) => {
+    const aDist = Math.abs(a.x - myHead.x) + Math.abs(a.y - myHead.y);
+    const bDist = Math.abs(b.x - myHead.x) + Math.abs(b.y - myHead.y);
+    return aDist - bDist;
+  });
+
+  console.log(isMoveSafe);
+  console.log(food);
+  console.log(myHead);
+
+  const path = astar(myHead, food[0], isMoveSafe);
+  console.log(path);
+
+  // for (let i = 0; i < food.length; i++) {
+  //   if (food[i].x === myHead.x - 1 && food[i].y === myHead.y && isMoveSafe.left) {
+  //     return { move: "left" };
+  //   } else if (food[i].x === myHead.x + 1 && food[i].y === myHead.y && isMoveSafe.right) {
+  //     return { move: "right" };
+  //   }
+
+  //   if (food[i].y === myHead.y - 1 && food[i].x === myHead.x && isMoveSafe.down) {
+  //     return { move: "down" };
+  //   } else if (food[i].y === myHead.y + 1 && food[i].x === myHead.x && isMoveSafe.up) {
+  //     return { move: "up" };
+  //   }
+  // }
 
   console.log(`MOVE ${gameState.turn}: ${nextMove}`)
   return { move: nextMove };
